11056020cbdc32295bb29dd758069669fabe4e60
kyr1no <dredgar065@gmail.com>
2017-07-11 00:07:46 +0800

    NOI ++rp;

---
+++ probs/bzoj1588_luogu2234_splay.cpp
@@ -0,0 +1,59 @@
+// splay
+#include <cstdio>
+#include <algorithm>
+const int MAXN = 32770, INF = 1000000200;
+
+int n, root;
+int val[MAXN], ln = 0; // last_node
+int fa[MAXN], ch[MAXN][2];
+void link(int o, int x, int d) { // make o the "d" child of x
+    if (o) fa[o] = x;
+    if (x) ch[x][d] = o;
+}
+int chd(int o) { // the child-type of o
+    return ch[fa[o]][1] == o;
+}
+void rotate(int o) {
+    int x = fa[o], d = chd(o);
+    link(o, fa[x], chd(x)); // Diffed
+    link(ch[o][d ^ 1], x, d);
+    link(x, o, d ^ 1); // Reverse the two is wrong
+    if (x == root) root = o; // Diffed
+}
+void splay(int o) { // So fun.
+    for (int x; (x = fa[o]); rotate(o)) 
+        if (fa[x]) rotate(chd(o) == chd(x) ? x : o);
+}
+int add_node(int v) {
+    val[++ln] = v;
+    return ln;
+}
+void insert(int o, int v) {
+    for (int x; (x = ch[o][v >= val[o]]); o = x);
+    link(add_node(v), o, v >= val[o]);
+    splay(ln); // Important
+}
+void init() {
+    root = add_node(INF);
+    insert(root, -INF); // provide bounds
+}
+int nb(int o, int d) { // d = 0 for pre, 1 for suf
+    o = ch[o][d];
+    for (d ^= 1; ch[o][d]; o = ch[o][d]); // Change?
+    return val[o];
+}
+
+int main() {
+    scanf("%d", &n);
+    init();
+    int i, v, ans = 0, pre, sur;
+    for (i = 1; i <= n; ++i) {
+        scanf("%d", &v);
+        insert(root, v);
+        pre = nb(root, 0);
+        sur = nb(root, 1);
+        ans += ((i == 1) ? v : (std::min(v - pre, sur - v)));
+    }
+    printf("%d\n", ans);
+    return 0;
+}
--- probs/luogu2704.cpp
+++ probs/luogu2704.cpp
@@ -1,83 +1,59 @@
 #include <cstdio>
-#include <algorithm>
 #define MAXN 105
 #define MAXM 15
 #define MAXS 1040
 
-
-void print_bin(int s) {
-    for (int i = 31; i >= 0; --i) {
-        printf("%d", (s >> i) & 1);
-    }
-    printf("\n");
-}
-
-
-int count(int s) {
-    int res = 0;
-    for (; s; s >>= 1)
-        if (s & 1) ++res;
-    return res;
-}
 int n, m, map[MAXN][MAXM], plains[MAXN];
 
 int states[MAXS], scnt = 1; // states[1] = {}
 void make_states() {
     int d, state;
-    for (int i = 2; i < (1 << (m + 1)); i += 2) {
+    for (int i = 1; i < 1 << m; ++i) {
         d = m + 10;
         state = i;
-        for (; state; state >>= 1) {
+        for (; state; state <<= 1) {
             ++d;
             if (state & 1) {
-                if (d <= 2) goto tag;
+                if (d <= 2) continue;
                 else d = 0;
             }
         }
         states[++scnt] = i;
-        tag:;
     }
 }
-
 
 int dp[MAXN][MAXS][MAXS];
 // dp[i][s][t] := 第 i 行状态为 s，第 j 行状态为 t 时前 i 行炮数最大值
 // dp[i][s][t] = max{dp[i - 1][t][k] | k in {states[]} } + |s|
 int main() {
     scanf("%d%d", &n, &m);
-    int i, s, t, k, ss, tt, kk;
-    int tmp, ans = 0;
-    char line[MAXM];
+    int i, s, t, k, ss, tt, kk, pla;
+    char tmp;
     for (i = 1; i <= n; ++i) {
-        scanf("%s", line);
         for (int j = 1; j <= m; ++j) {
-            tmp = line[j - 1];
+            scanf("%c", &tmp);
             if (tmp == 'P') {
-                plains[i] |= (1 << j);
+                plains[i] &= (1 << j);
             }
         }
+        scanf("%c", &tmp);
     }
     make_states();
     for (i = 1; i <= n; ++i) {
         for (s = 1; s <= scnt; ++s) {
             ss = states[s];
-            if ((ss | plains[i]) != plains[i]) continue;
+            if (ss & plains[i] != plains[i]) continue;
             for (t = 1; t <= scnt; ++t) {
                 tt = states[t];
-                if ((tt | plains[i - 1]) != plains[i - 1]) continue; // as plains[0] = {}
+                if (tt & plains[i - 1] != plains[i - 1]) continue; // as plains[0] = {}
                 if (ss & tt) continue;
                 for (k = 1; k <= scnt; ++k) {
-                    kk = states[k];
-                    tmp = plains[((i - 2) >= 0) ? (i - 2) : (i - 1)];
-                    if ((kk | tmp) != tmp) continue;
-                    if ((kk & ss) || (kk & tt)) continue;
-                    dp[i][s][t] = std::max(dp[i][s][t], dp[i - 1][t][k] + count(s));
-                    if (i == n) ans = std::max(ans, dp[i][s][t]);
+                    kk = states[k];                    
+                    if (kk & ss || kk & tt) continue;
                 }
             }
         }
     }
-    printf("%d\n", ans);
 
 
 }
