d8fb02bcd2f74c3f906697c1b12d9330bee7081a
kyr1no <dredgar065@gmail.com>
2017-10-09 01:26:56 +0800

    Commits from gacp

---
+++ probs/bzoj4591.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+
+typedef long long ll;
+
+const int p = 2333;
+int c[p + 5][p + 5], s[p + 5][p + 5];
+void init() {
+    static int i, j;
+    c[0][0] = 1;
+    for (i = 1; i < p; ++i) {
+        c[i][0] = 1;
+        for (j = 1; j <= i; ++j) {
+            c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
+            if (c[i][j] >= p)
+                c[i][j] -= p;
+        }
+    }
+    for (i = 0; i < p; ++i) {
+        s[i][0] = 1;
+        for (j = 1; j < p; ++j) {
+            s[i][j] = s[i][j - 1] + c[i][j];
+            if (s[i][j] >= p)
+                s[i][j] -= p;
+        }
+    }
+}
+int lucas(ll n, ll k) {
+    if (k == 0)
+        return 1;
+    if (k > n)
+        return 0;
+    if (n < p && k < p)
+        return c[n][k];
+    return c[n % p][k % p] * lucas(n / p, k / p) % p;
+}
+int solve(ll n, ll k) {
+    if (k == 0)
+        return 1;
+    if (k < 0)
+        return 0;
+    int nmp = n % p, kmp = k % p, res, tmp, tmp2;
+    ll ndp = n / p, kdp = k / p;
+    tmp = s[nmp][p - 1] - s[nmp][kmp];
+    if (tmp < 0)
+        tmp += p;
+    tmp2 = solve(ndp, kdp);
+    res = s[nmp][kmp] * (tmp2 + lucas(ndp, kdp)) % p + tmp * tmp2 % p;
+    // res = s[nmp][kmp] * tmp2 % p + tmp * (tmp2 + lucas(ndp, kdp + 1)) % p;
+    if (res >= p)
+        return res - p;
+    return res;
+}
+
+int main() {
+    static int t, kase;
+    static ll n, k;
+    init();
+    scanf("%d", &t);
+    for (kase = 0; kase < t; ++kase) {
+        scanf("%lld%lld", &n, &k);
+        printf("%d\n", solve(n, k));
+    }
+
+    return 0;
+}
