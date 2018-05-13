179e0ab138b5d578cf4ea59e08c3eafae2b85c95
kyr1no <dredgar065@gmail.com>
2018-05-13 22:39:44 +0800

    from gacp

--- probs/bzoj5140-bfs-set-magic-bf.cpp
+++ probs/bzoj5140-bfs-set-magic-bf.cpp
@@ -2,8 +2,6 @@
 #include <cctype>
 #include <algorithm>
 #include <queue>
-#include <bitset>
-#include <vector>
 #define rep(__i,__s,__t) for((__i)=(__s);(__i)<=(__t);++(__i))
 #define re(__i,__s,__t) for((__i)=(__s);(__i)<(__t);++(__i))
 #define per(__i,__s,__t) for((__i)=(__s);(__i)>=(__t);--(__i))
@@ -74,41 +72,63 @@ struct IO {
     }
 } io;
 
-const int N = 1003, M = 10003;
-int deg[N];
-std::vector<int> g[N];
-std::bitset<1001> d[N];
+const int N = 100002;
+struct Edge {
+    int v;
+    Edge *e;
+} pool[N * 100], *curr = pool, *g[N * 2];
+void arc(int u, int v) {
+    g[u] = new (curr++) Edge{v, g[u]};
+}
+int n, nn, k, a[N * 2], b[N * 2], d[N * 2];
 std::queue<int> q;
-
-int main() { // answer should be at the worst case
-    static int n, m, i, ans, u, v;
+std::pair<int, int> l[N], *p;
+int main() {
+    static int i, u, v, nd;
     n = io;
-    m = io;
-    rep (i, 1, m) {
-        u = io;
-        v = io;
-        g[u].push_back(v);
-        ++deg[v];
+    k = io;
+    nn = n + n;
+    rep (i, 1, n) {
+        a[i] = io;
+        b[i] = io;
+        if (b[i] == 0)
+            q.push(i), d[i] = 1;
+    }
+    rep (i, n + 1, nn) {
+        a[i] = io;
+        b[i] = io;
+        if (a[i] == 0)
+            q.push(i), d[i] = 1;
     }
     rep (i, 1, n)
-        if (!deg[i])
-            q.push(i);
+        l[i] = std::make_pair(b[n + i], n + i);
+    std::sort(l + 1, l + n + 1);
+    rep (i, 1, n) {
+        p = std::lower_bound(l + 1, l + n + 1, std::make_pair(b[i], 0));
+        while (p <= l + n && b[i] <= p->first && p->first <= b[i] + k)
+            arc((p++)->second, i);
+    }
+    rep (i, 1, n)
+        l[i] = std::make_pair(a[i], i);
+    std::sort(l + 1, l + n + 1);
+    rep (i, n + 1, nn) {
+        p = std::lower_bound(l + 1, l + n + 1, std::make_pair(a[i], 0));
+        while (p <= l + n && a[i] <= p->first && p->first <= a[i] + k)
+            arc((p++)->second, i);
+    }
+
+    static Edge *e;
     while (!q.empty()) {
-        static std::vector<int>::iterator it, ed;
-        std::bitset<1001> &du = d[u];
         u = q.front();
         q.pop();
-        ans += du.count();
-        ed = g[u].end();
-        for (it = g[u].begin(); it != ed; ++it) {
-            v = *it;
-            d[v] |= du;   // use |=, not =  ****************
-            d[v].set(u);
-            if (--deg[v] == 0)
-                q.push(v);
+        nd = d[u] + 1;
+        for (e = g[u]; e; e = e->e) if (d[v = e->v] == 0) {
+            d[v] = nd;
+            q.push(v);
         }
     }
-    io.print(((n * (n - 1)) >> 1) - ans);
+    rep (i, 1, n)
+        io.print(d[i] ? d[i] : -1);
 
     io.flush();
     return 0;
