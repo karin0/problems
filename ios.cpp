bb4c8f730888511026eaf5a9d6f9feeb7bbf38a2
kyr1no <dredgar065@gmail.com>
2018-05-23 02:02:44 +0800

    >_<

--- ios.cpp
+++ ios.cpp
@@ -1,29 +1,22 @@
-#include <cstdio>
-#include <cctype>
-#include <algorithm>
+#include <bits/stdc++.h>
 #define rep(__i,__s,__t) for((__i)=(__s);(__i)<=(__t);++(__i))
 #define re(__i,__s,__t) for((__i)=(__s);(__i)<(__t);++(__i))
 #define per(__i,__s,__t) for((__i)=(__s);(__i)>=(__t);--(__i))
 #define pe(__i,__s,__t) for((__i)=(__s);(__i)>(__t);--(__i))
+#ifdef AKARI
+    #define ccc(x) std::cerr << #x " = " << x << "  "
+    #define cccc(x) std::cerr << #x " = " << x << std::endl
+    #define ccccc(x) std::cerr << x << std::endl
+#else
+    #define ccc(x) 0
+    #define cccc(x) 0
+    #define ccccc(x) 0
+#endif
 
-struct IO{static const int L=1000000;char a[L],b[L],*s,*t,*p;IO():p(b){}inline char gc(){if (s == t)t = (s = a) + fread(a, 1, L, stdin);return *s++; /* return s == t ? EOF : *s++;*/}void gs(char *st) {static char c;for (c = gc(); !isgraph(c); c = gc());*st++ = c;for (c = gc(); isgraph(c); c = gc())*st++ = c;*st++ = 0;}template <class T>operator T() {static T x;static char c;static bool neg;for (c = gc(); c != '-' && !isdigit(c); c = gc());if (c == '-')neg = true, c = gc();else neg = false;x = c - '0';for (c = gc(); isdigit(c); c = gc())x = x * 10 + (c - '0');return neg ? -x : x;}inline void pc(const char c) {if (p == b + L)fwrite(p = b, 1, L, stdout);*p++ = c;}template<class T>void print(T x, const bool nl = true) {static char c[30], *q;static T y;if (x == 0)pc('0');else{if (x < 0)pc('-'), x = -x;for (q = c; x; x = y)y = x / 10, *q++ = x - y * 10 + '0';while (q != c)pc(*--q);}
-        if (nl)
-            pc('\n');
-    }
-    void ps(const char *st, const bool nl = true) {
-        while (*st)
-            pc(*st++);
-        if (nl)
-            pc('\n');
-    }
-    inline void flush() const {
-        fwrite(b, 1, p - b, stdout); // p = b;
-    }
-} io;
+struct IO{static const int L=1000000;char a[L],b[L],*s,*t,*p;IO():p(b){}~IO(){flush();}char gc(){if(s==t)t=(s=a)+fread(a,1,L,stdin);return*s++;/*return s==t?EOF:*s++;*/}void gs(char*st){static char c;for(c=gc();!isgraph(c);c=gc());*st++=c;for(c=gc();isgraph(c);c=gc())*st++=c;*st++=0;}template<class T>operator T(){static T x;static char c;static bool neg;for(c=gc();c!='-'&&!isdigit(c);c=gc());if(c=='-')neg=true,c=gc();else neg=false;x=c-'0';for(c=gc();isdigit(c);c=gc())x=x*10+(c-'0');return neg?-x:x;}void pc(const char c){if(p==b+L)fwrite(p=b,1,L,stdout);*p++=c;}template<class T>void print(T x,const char end='\n'){static char c[30],*q;static T y;if(x==0)pc('0');else{if(x<0)pc('-'),x=-x;for(q=c;x;x=y)y=x/10,*q++=x-y*10+'0';while(q!=c)pc(*--q);}if(end)pc(end);}void ps(const char*st,const char end='\n'){while(*st)pc(*st++);if(end)pc(end);}void flush()const{fwrite(b,1,p-b,stdout);/*p=b;*/}}io;
 
 int main() {
     static int i, x;
 
-    io.flush();
     return 0;
 }
