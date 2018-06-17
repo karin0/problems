5cc5c31023ea1cc6b258e067ef8307b3d25fac91
kyr1no <dredgar065@gmail.com>
2018-06-17 15:51:25 +0800

    remove all

--- probs/contests/cf/Round #425 (Div. 2)/a.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-long long n, k;
-int main() {
-    std::cin >> n >> k;
-    std::cout << (((n / k) & 1) ? "YES\n" : "NO\n");
-
-    return 0;
-}
--- probs/contests/cf/Round #431 (Div. 2)/test.py
+++
@@ -1,5 +0,0 @@
-for i in range(1000):
-    y = i * (i - 1) / 2
-    print(i, y)
-    if y > 100000:
-        break
